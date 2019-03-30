6d335f7967069714546de36ac5456e6573b53f80
Christian Hockenberger <christian@mediaberger.com>
2019-03-30 11:58:06 +0100

    Footer styles

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -9,10 +9,10 @@
 
 			<?php do_action( 'themeberger_before_footer' ); ?>
 			<footer id="colophon" class="site-footer">
-				<div class="site-info">
-					site info
-				</div><!-- .site-info -->
 				<div class="site-footer--inner">
+					<div class="site-info">
+						site info
+					</div><!-- .site-info -->
 					<div class="site-footer--left">
 						footer left
 					</div>
