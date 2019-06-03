a1667e01757a1e8918ccfcbd8d0f0af5e0b1158b
Christian Hockenberger <christian@mediaberger.com>
2019-06-03 11:04:06 +0200

    Interaction indicators with unicode emojis 📯

--- template-parts/content-post-audio.php
+++ template-parts/content-post-audio.php
@@ -42,65 +42,7 @@ $approved_comments = $comments_count->approved;
 			<p>Shortlink: <?php the_shorturl(); ?></p>
 			<?php the_tags( '<p>Tags: ', ', ', '</p>' ); ?>
 			<?php endif; ?>
-			<?php if ( function_exists( 'get_linkbacks_number' ) ) : ?>
-				<?php if ( $approved_comments > 0 ) : ?>
-				<ul class="entry-interactions">
-					<li class="interaction"><strong><?php echo esc_attr( $approved_comments ); ?> Interactions</strong></li>
-					<?php if ( esc_attr( $approved_comments ) - get_linkbacks_number() > 0 ) : ?>
-					<li class="interaction"><span class="interaction-key key-comment">Comments</span><strong class="interaction-value value-comment"><?php echo ( esc_attr( $approved_comments ) - get_linkbacks_number() ); ?></strong></li>
-					<?php endif; ?>
-					<?php if ( get_linkbacks_number( 'reply' ) ) : ?>
-					<li class="interaction"><span class="interaction-key key-reply">Replies</span><strong class="interaction-value value-reply"><?php echo get_linkbacks_number( 'reply' ); ?></strong></li>
-					<?php endif; ?>
-					<?php if ( get_linkbacks_number( 'repost' ) ) : ?>
-					<li class="interaction"><span class="interaction-key key-repost">Reposts</span><strong class="interaction-value value-repost"><?php echo get_linkbacks_number( 'repost' ); ?></strong></li>
-					<?php endif; ?>
-					<?php if ( get_linkbacks_number( 'like' ) ) : ?>
-					<li class="interaction"><span class="interaction-key key-like">Likes</span><strong class="interaction-value value-like"><?php echo get_linkbacks_number( 'like' ); ?></strong></li>
-					<?php endif; ?>
-					<?php if ( get_linkbacks_number( 'favorite' ) ) : ?>
-					<li class="interaction"><span class="interaction-key key-favorite">Favorites</span><strong class="interaction-value value-favorite"><?php echo get_linkbacks_number( 'favorite' ); ?></strong></li>
-					<?php endif; ?>
-					<?php if ( get_linkbacks_number( 'bookmark' ) ) : ?>
-					<li class="interaction"><span class="interaction-key key-bookmark">Bookmarks</span><strong class="interaction-value value-bookmark"><?php echo get_linkbacks_number( 'bookmark' ); ?></strong></li>
-					<?php endif; ?>
-					<?php if ( get_linkbacks_number( 'rsvp' ) ) : ?>
-					<li class="interaction"><span class="interaction-key key-rsvp">RSVP</span><strong class="interaction-value value-rsvp"><?php echo get_linkbacks_number( 'rsvp' ); ?></strong></li>
-					<?php endif; ?>
-					<?php if ( get_linkbacks_number( 'rsvp:yes' ) ) : ?>
-					<li class="interaction"><span class="interaction-key key-rsvp-yes">RSVP Yes</span><strong class="interaction-value value-rsvp-yes"><?php echo get_linkbacks_number( 'rsvp:yes' ); ?></strong></li>
-					<?php endif; ?>
-					<?php if ( get_linkbacks_number( 'rsvp:no' ) ) : ?>
-					<li class="interaction"><span class="interaction-key key-rsvp-no">RSVP No</span><strong class="interaction-value value-rsvp-no"><?php echo get_linkbacks_number( 'rsvp:no' ); ?></strong></li>
-					<?php endif; ?>
-					<?php if ( get_linkbacks_number( 'rsvp:maybe' ) ) : ?>
-					<li class="interaction"><span class="interaction-key key-rsvp-maybe">RSVP Maybe</span><strong class="interaction-value value-rsvp-maybe"><?php echo get_linkbacks_number( 'rsvp:maybe' ); ?></strong></li>
-					<?php endif; ?>
-					<?php if ( get_linkbacks_number( 'rsvp:interested' ) ) : ?>
-					<li class="interaction"><span class="interaction-key key-rsvp-interested">RSVP Interested</span><strong class="interaction-value value-rsvp-interested"><?php echo get_linkbacks_number( 'rsvp:interested' ); ?></strong></li>
-					<?php endif; ?>
-					<?php if ( get_linkbacks_number( 'invited' ) ) : ?>
-					<li class="interaction"><span class="interaction-key key-invited">Invited</span><strong class="interaction-value value-invited"><?php echo get_linkbacks_number( 'invited' ); ?></strong></li>
-					<?php endif; ?>
-					<?php if ( get_linkbacks_number( 'listen' ) ) : ?>
-					<li class="interaction"><span class="interaction-key key-listen">Listen</span><strong class="interaction-value value-listen"><?php echo get_linkbacks_number( 'listen' ); ?></strong></li>
-					<?php endif; ?>
-					<?php if ( get_linkbacks_number( 'watch' ) ) : ?>
-					<li class="interaction"><span class="interaction-key key-watch">Watch</span><strong class="interaction-value value-watch"><?php echo get_linkbacks_number( 'watch' ); ?></strong></li>
-					<?php endif; ?>
-					<?php if ( get_linkbacks_number( 'read' ) ) : ?>
-					<li class="interaction"><span class="interaction-key key-read">Read</span><strong class="interaction-value value-read"><?php echo get_linkbacks_number( 'read' ); ?></strong></li>
-					<?php endif; ?>
-					<?php if ( get_linkbacks_number( 'follow' ) ) : ?>
-					<li class="interaction"><span class="interaction-key key-follow">Follow</span><strong class="interaction-value value-follow"><?php echo get_linkbacks_number( 'follow' ); ?></strong></li>
-					<?php endif; ?>
-				</ul>
-				<?php endif; ?>
-			<?php else: ?>
-				<?php if ( $approved_comments > 0 ) : ?>
-				<p>Comments: <?php echo esc_attr( $approved_comments ); ?></p>
-				<?php endif; ?>
-			<?php endif; ?>
+			<?php get_template_part( 'template-parts/partial-interactions', 'audio' ); ?>
 		</footer><!-- .entry-footer -->
 		<?php endif; ?>
 
