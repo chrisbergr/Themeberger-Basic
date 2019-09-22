a95145f91c455ada665dde27e35d0b61ea94a1ad
Christian Hockenberger <christian@mediaberger.com>
2019-09-22 23:11:01 +0200

    Moved "p-name" to the footer for "light post types"

--- template-parts/partial-entry-footer-single.php
+++ template-parts/partial-entry-footer-single.php
@@ -8,7 +8,7 @@
  */
 ?>
 
-	<p><?php the_title( '<strong class="meta-title">', '</strong> | ' ); ?><span itemprop="articleSection"><?php the_category( ', ' ); ?></span><?php the_permalink_date( ' | ', '', false ); ?></p>
+	<p><?php the_title( '<strong class="meta-title p-name">', '</strong> | ' ); ?><span itemprop="articleSection"><?php the_category( ', ' ); ?></span><?php the_permalink_date( ' | ', '', false ); ?></p>
 	<?php do_action( 'themeberger_entry_footer' ); ?>
 	<p>Shortlink: <?php the_shorturl(); ?></p>
 	<?php the_tags( '<p>Tags: <span itemprop="keywords">', ', ', '</span></p>' ); ?>
